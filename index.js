214dfb02f2222d804d1866fac9d049cb5c436cac
ridoy113 <almamounhridoy113@gmail.com>
2022-04-27 00:37:45 +0600

    end hero

--- index.js
+++ index.js
@@ -121,6 +121,11 @@ app.get('/', (req, res) => {
     res.send('Running Genius Server');
 });
 
+app.get('/hero', (req, res) => {
+    res.send('Hero meets hero ku');
+});
+
+
 app.listen(port, () => {
     console.log('Listening to port', port);
 })
